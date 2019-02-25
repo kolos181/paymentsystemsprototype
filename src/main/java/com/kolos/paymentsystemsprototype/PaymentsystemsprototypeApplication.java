420e1f2bca96c046fd0627eba622f36d7e25ea5c
Kolos <kolos181@gmail.com>
2019-02-25 18:09:49 +0200

    Initial commit

---
+++ src/main/java/com/kolos/paymentsystemsprototype/PaymentsystemsprototypeApplication.java
@@ -0,0 +1,13 @@
+package com.kolos.paymentsystemsprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsystemsprototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsystemsprototypeApplication.class, args);
+    }
+
+}
